cbbb60953f983effb80897fcf4f3790a8e53bf75
Justine Munoz <justineemunozz@gmail.com>
2025-06-28 22:45:50 +0200

    registration done

--- matcha/src/app/registration/page.tsx
+++ matcha/src/app/registration/page.tsx
@@ -38,7 +38,7 @@ export default function RegistrationPage() {
         <div className="mt-3 text-3xl font-extrabold text-neutral mb-2">
           Registration
         </div>
-        <div className="bg-base-100/95 shadow-lg w-screen px-6 py-8 overflow-y-auto md:card md:w-[430px] flex flex-col items-center gap-4 md:gap-6 md:my-8 h-[75vh] md:h-auto md:overflow-hidden">
+        <div className="bg-base-100/95 shadow-lg w-screen px-6 py-8 overflow-y-auto md:card md:w-[430px] flex flex-col items-center gap-4 md:gap-6 h-[75vh] md:h-auto md:overflow-hidden">
           <Field
             label="E-mail"
             type="email"
@@ -63,37 +63,12 @@ export default function RegistrationPage() {
             }
             hint="Enter valid email address"
           />
-          {/* <fieldset className="fieldset">
-            <legend className="fieldset-legend">E-mail</legend>
-            <label className="input validator w-xs">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
-              <input type="email" placeholder="mail@site.com" required />
-            </label>
-            <div className="validator-hint hidden">
-              Enter valid email address
-            </div>
-          </fieldset> */}
           <Field
             label="Username"
             type="text"
             placeholder="Username"
             pattern="[A-Za-z][A-Za-z0-9\-]*"
-            title="Only letters, numbers or dash"
+            title="Only letters, numbers or hyphen (-) allowed"
             icon={
               <svg
                 className="h-[1.2em] opacity-50"
@@ -112,43 +87,8 @@ export default function RegistrationPage() {
                 </g>
               </svg>
             }
-            hint="3-30 caractères : lettres, chiffres ou tiret"
+            hint="3-30 characters: letters, numbers or hyphen (-)"
           />
-          {/* <fieldset className="fieldset">
-            <legend className="fieldset-legend">Username</legend>
-            <label className="input validator w-xs">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-                  <circle cx="12" cy="7" r="4"></circle>
-                </g>
-              </svg>
-              <input
-                type="text"
-                required
-                placeholder="Username"
-                pattern="[A-Za-z][A-Za-z0-9\-]*"
-                // minLength="3"
-                // maxLength="30"
-                title="Only letters, numbers or dash"
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be 3 to 30 characters
-              <br />
-              Containing only letters, numbers or dash
-            </p>
-          </fieldset> */}
           <Field
             label="Firstname"
             type="text"
@@ -156,15 +96,6 @@ export default function RegistrationPage() {
             icon={null}
             hint=""
           />
-          {/* <fieldset className="fieldset">
-            <legend className="fieldset-legend">Firstname</legend>
-            <input
-              type="text"
-              required
-              className="input w-xs"
-              placeholder="Firstname"
-            />
-          </fieldset> */}
           <Field
             label="Lastname"
             type="text"
@@ -172,15 +103,6 @@ export default function RegistrationPage() {
             icon={null}
             hint=""
           />
-          {/* <fieldset className="fieldset">
-            <legend className="fieldset-legend">Lastname</legend>
-            <input
-              type="text"
-              required
-              className="input w-xs"
-              placeholder="Lastname"
-            />
-          </fieldset> */}
           <Field
             label="Password"
             type="password"
@@ -208,51 +130,10 @@ export default function RegistrationPage() {
                 </g>
               </svg>
             }
-            title="8+ caractères, 1 minuscule, 1 majuscule, 1 chiffre minimum"
+            title="8+ characters, 1 lowercase, 1 uppercase, 1 number minimum"
             pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-            hint="8+ caractères, minuscule, majuscule et chiffre"
+            hint="8+ characters, lowercase, uppercase and number"
           />
-          {/* <fieldset className="fieldset">
-            <legend className="fieldset-legend">Lastname</legend>
-            <label className="input validator w-xs">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                  <circle
-                    cx="16.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </g>
-              </svg>
-              <input
-                type="password"
-                required
-                placeholder="Password"
-                // minlength="8"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be more than 8 characters, including:
-              <br />
-              At least one number <br />
-              At least one lowercase letter <br />
-              At least one uppercase letter
-            </p>
-          </fieldset> */}
           <button
             type="submit"
             className="btn btn-primary shadow-lg font-bold
@@ -260,7 +141,6 @@ export default function RegistrationPage() {
           >
             Register
           </button>
-          {/* </div> */}
         </div>
       </div>
     </div>

6c29bd2994994efeec6157811c6a4e627e0aa91c
Vodki <sylvestre.louis26@outlook.fr>
2025-07-24 05:03:05 +0200

    migrations init

---
+++ backend/migrations/20250724024851_users_table_add.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id            SERIAL PRIMARY KEY,
+    email         TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    first_name    TEXT NOT NULL,
+    last_name     TEXT NOT NULL,
+    username      TEXT UNIQUE NOT NULL,
+    created_at    TIMESTAMP NOT NULL DEFAULT NOW(),
+    gender        TEXT,
+    orientation   TEXT,
+    birthday      DATE,
+    bio           TEXT,
+    avatar_url    TEXT
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd

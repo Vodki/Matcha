2677c228cfcc3ad71b3961de4a7afcadb3b47ac2
Justine MUNOZ <justinemunoz@carbonriskintelligence.fr>
2025-09-04 16:07:34 +0200

    last fix for the nav dropdown

--- matcha/src/app/home/profile/page.tsx
+++ matcha/src/app/home/profile/page.tsx
@@ -5,9 +5,9 @@ import GeneralInformations from "../../../components/profile/GeneralInformations
 
 export default function Profile() {
   return (
-    <div className="flex flex-col gap-2 w-full  mx-auto">
+    <div className="flex flex-col gap-2 w-full mx-auto">
       <div className="mt-20 text-xl font-extrabold ms-2">Profile</div>
-      <div className="bg-base-100/95 shadow-lg w-screen px-6 py-8 h-[75vh] lg:h-auto overflow-y-auto lg:card flex flex-col lg:flex-row gap-4 lg:gap-6">
+      <div className="bg-base-100/95 shadow-lg w-full px-6 py-8 h-[75vh] lg:h-auto overflow-y-auto lg:card flex flex-col lg:flex-row gap-4 lg:gap-6">
         <div className="flex-1 min-w-0">
           <GeneralInformations />
         </div>

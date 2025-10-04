0bb9e311dcdd224f33c32be18275fc2880bddd71
Vodki <sylvestre.louis26@outlook.fr>
2025-10-04 22:50:33 +0200

    logout button fix

--- matcha/src/app/home/layout.tsx
+++ matcha/src/app/home/layout.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useGlobalAppContext } from "../../contexts/GlobalAppContext";
 import NotificationBell from "../../components/profile/NotificationBell";
+import LogoutButton from "@/components/LogoutButton";
 
 export default function RootLayout({
   children,
@@ -113,26 +114,7 @@ export default function RootLayout({
         </div>
 
         <div className="navbar-end">
-          <Link
-            href="/"
-            className="btn btn-ghost text-sm flex items-center gap-2"
-          >
-            <div className="mt-0.5">Log Out</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h6a2 2 0 012 2v1"
-              />
-            </svg>
-          </Link>
+          <LogoutButton />
         </div>
       </div>
 
